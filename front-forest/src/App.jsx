b6aa1599c099b09dadf052a50be62ab329be515b
Mateo Vasco <macv9620@utp.edu.co>
2023-11-15 19:38:14 -0500

    Se crea form para sign up

--- front-forest/src/App.jsx
+++ front-forest/src/App.jsx
@@ -5,6 +5,7 @@ import { Login } from './Pages/Login'
 import { LoadingSpinner } from './Components/Spinner/Spinner'
 import { useAppContext } from './Context/AppContextProvider'
 import { SavedQueries } from './Pages/SavedQueries'
+import { SignUp } from './Pages/SignUp'
 
 function App () {
   const { showLoadingSpinner } = useAppContext()
@@ -13,6 +14,7 @@ function App () {
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Login />} />
+          <Route path='/signUp' element={<SignUp />} />
           <Route path='/dashboard' element={<DashBoard />} />
           <Route path='/savedQueries' element={<SavedQueries />} />
         </Routes>
